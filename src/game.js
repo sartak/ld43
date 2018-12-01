cf45185af64b95007745be440f470caaccdaddb7
Shawn M Moore <code@sartak.org>
2018-12-01 00:09:58 -0500

    add enemies etc

--- src/game.js
+++ src/game.js
@@ -1,8 +1,12 @@
 /* @flow */
 import Phaser from 'phaser';
 import heroSprite from './assets/hero.png';
+
+import enemySpriteA from './assets/enemy-a.png';
+
 import skyImage from './assets/sky.png';
 import groundImage from './assets/ground.png';
+
 import physicsShapes from './assets/physics.json';
 
 // SACRIFICES MUST BE MADE
@@ -20,7 +24,7 @@ const config = {
   physics: {
     default: 'matter',
     matter: {
-      debug: DEBUG,
+      debug: false,
     },
   },
   scene: {
@@ -32,6 +36,7 @@ const config = {
 
 const state : any = {
   physicsShapes,
+  enemies: [],
 };
 
 if (DEBUG) {
@@ -45,6 +50,7 @@ export default function startGame() {
 function preload() {
   const game = state.game = this;
   game.load.image('hero', heroSprite);
+  game.load.image('enemy-a', enemySpriteA);
   game.load.image('sky', skyImage);
   game.load.image('ground', groundImage);
 }
@@ -57,7 +63,7 @@ function createHero(x, y) {
   const { Body, Bodies } = Phaser.Physics.Matter.Matter;
   const { width: w, height: h } = hero;
 
-  const sensors = {
+  const sensors = hero.sensors = {
     b: Bodies.rectangle(0, h * 0.5 + 2, w * 0.5, 2, { isSensor: true }),
     l: Bodies.rectangle(-w * 0.5, 0, 2, h * 0.5, { isSensor: true }),
     r: Bodies.rectangle(w * 0.5, 0, 2, h * 0.5, { isSensor: true }),
@@ -77,7 +83,22 @@ function createHero(x, y) {
   // can't rotate
   hero.setFixedRotation();
 
+  hero.touching = {
+    left: false,
+    right: false,
+    ground: false,
+  };
+
   return hero;
+}
+
+function createEnemy(type, x, y) {
+  const { game } = state;
+  const enemyId = `enemy-${type}`;
+
+  const enemy = game.matter.add.sprite(x, y, enemyId, null, { shape: physicsShapes[enemyId] });
+
+  return enemy;
 }
 
 function create() {
@@ -88,9 +109,19 @@ function create() {
 
   game.add.sprite(400, 300, 'sky');
   const ground = state.ground = matter.add.sprite(400, config.height - (config.groundHeight/2), 'ground', null, { shape: physicsShapes.ground });
+  ground.name = 'ground';
 
   // +20 for a little bit of dynamism on load
-  const hero = state.hero = createHero(100, 20 + config.height - (config.characterHeight/2));
+  const characterY = 20 + config.height - (config.characterHeight/2);
+  const hero = state.hero = createHero(100, characterY);
+
+  // target, round pixels for jitter, lerpx, lerpy, offsetx, offsety
+  game.cameras.main.startFollow(hero, false, 0.05, 0, 0, 270);
+
+  for (let i = 0; i < 4; ++i) {
+    const enemy = createEnemy('a', Phaser.Math.Between(100, 200) + 100 * i, characterY);
+    state.enemies.push(enemy);
+  }
 
   state.cursors = game.input.keyboard.createCursorKeys();
 
@@ -103,19 +134,87 @@ function create() {
       }
     });
   }
+
+  game.matter.world.on('collisionstart', (event) => {
+    event.pairs.forEach(({ bodyA, bodyB, separation }) => {
+      const a = bodyA.gameObject;
+      const b = bodyB.gameObject;
+
+      if (bodyA.id === hero.sensors.l.id || bodyB.id === hero.sensors.l.id) {
+        hero.touching.left = true;
+        hero.x += separation + 2;
+      }
+
+      if (bodyA.id === hero.sensors.r.id || bodyB.id === hero.sensors.r.id) {
+        hero.touching.right = true;
+        hero.x -= separation + 2;
+      }
+
+      if (!a || !b) {
+        return;
+      }
+
+      if ((bodyA.id === hero.sensors.b.id || bodyB.id === hero.sensors.b.id)) {
+        hero.touching.bottom = true;
+      }
+    });
+  });
+
+  game.matter.world.on('collisionend', (event) => {
+    event.pairs.forEach(({ bodyA, bodyB, separation }) => {
+      const a = bodyA.gameObject;
+      const b = bodyB.gameObject;
+
+      if (bodyA.id === hero.sensors.l.id || bodyB.id === hero.sensors.l.id) {
+        hero.touching.left = false;
+      }
+
+      if (bodyA.id === hero.sensors.r.id || bodyB.id === hero.sensors.r.id) {
+        hero.touching.right = false;
+      }
+
+      if (!a || !b) {
+        return;
+      }
+
+      if ((bodyA.id === hero.sensors.b.id || bodyB.id === hero.sensors.b.id)) {
+        hero.touching.bottom = false;
+      }
+    });
+  });
 }
 
 // parameter t is milliseconds since load
 function update() {
+  updateHero();
+}
+
+function updateHero() {
   const { hero, cursors } = state;
+
+  const { velocity } = hero.body;
   if (cursors.left.isDown) {
-    hero.x -= 10;
+    hero.applyForce({
+      x: -0.1,
+      y: 0,
+    });
   }
+
   if (cursors.right.isDown) {
-    hero.x += 10;
+    hero.applyForce({
+      x: 0.1,
+      y: 0,
+    });
   }
-  if (cursors.up.isDown) {
-    hero.y -= 10;
+
+  if (hero.touching.bottom && cursors.up.isDown) {
+    hero.applyForce({
+      x: 0,
+      y: -0.25,
+    });
   }
+
+  if (velocity.x > 5) hero.setVelocityX(5);
+  else if (velocity.x < -5) hero.setVelocityX(-5);
 }
 
