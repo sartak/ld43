4659905a0cea968a18585f7c8a1133350114ff6a
Shawn M Moore <code@sartak.org>
2018-11-30 21:52:33 -0500

    hero moving on screen

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 /* @flow */
 import Phaser from 'phaser';
+import heroSprite from './assets/hero.png';
 
 // SACRIFICES MUST BE MADE
 
@@ -9,15 +10,51 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: {
-        y: 200,
-      },
+    default: 'matter',
+    matter: {
+      debug: true,
     },
   },
+  scene: {
+    preload,
+    create,
+    update,
+  },
 };
+
+const state = {};
+window.state = state;
 
 export default function startGame() {
   return new Phaser.Game(config);
 }
+
+function preload() {
+  const game = state.game = this;
+  game.load.image('hero', heroSprite);
+}
+
+function create() {
+  const { game } = state;
+
+  const hero = state.hero = game.add.image(400, 300, 'hero');
+
+  state.cursors = game.input.keyboard.createCursorKeys();
+}
+
+function update() {
+  const { hero, cursors } = state;
+  if (cursors.left.isDown) {
+    hero.x -= 10;
+  }
+  if (cursors.right.isDown) {
+    hero.x += 10;
+  }
+  if (cursors.up.isDown) {
+    hero.y -= 10;
+  }
+  if (cursors.down.isDown) {
+    hero.y += 10;
+  }
+}
+
