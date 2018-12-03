507a57faef90015639fd4899698ec3a3e6ddda65
Shawn M Moore <code@sartak.org>
2018-12-02 23:21:55 -0500

    No more streaming

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import Stream from './Stream';
+// import Stream from './Stream';
 import Deployed from './Deployed';
 
-const Layout =
-  !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
-    ? Stream
-    : Deployed;
+const Layout = Deployed;
+//    ? Stream
 
 ReactDOM.render(<Layout />, document.getElementById('root'));
